53728bb5ea6bbe7e7e09de5877ed1b404822d86a
Lautaro Chaparro <lautaro.chaparro.et21.21@gmail.com>
2024-05-07 09:28:43 -0300

    add cors origin

--- src/presentation/app.js
+++ src/presentation/app.js
@@ -20,7 +20,10 @@ app.set('PORT', PORT)
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors({
+  credentials: true,
+  origin: '*'
+}))
 
 app.use('/auth', authRoutes)
 app.use('/player', playerRoutes)
