a85cb89dbea92a980aa31b45beaaa7dc55c2b44a
Lautaro Chaparro <lautaro.chaparro.et21.21@gmail.com>
2024-05-14 23:04:47 -0300

    nashe

--- src/domain/controllers/auth.admin.controllers.js
+++ src/domain/controllers/auth.admin.controllers.js
@@ -2,7 +2,7 @@ const UserAdminModel = require('../../data/models/user.admin.model')
 
 async function login(req, res) {
   const { email, password } = req.body
-  const userFound = await UserAdminModel.findUserByEmail({email})
+  const userFound = await UserAdminModel.findUserByEmail({ email })
   if (userFound && userFound.password === password)
     return res.status(200).json({ state: true })
   return res.status(400).json({ state: false })
