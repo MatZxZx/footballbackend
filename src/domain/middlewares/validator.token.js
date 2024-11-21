9ba66cde31c0535d4bd4eba85f843718fd5fab4f
Mat^ <143543189+MatZxZx@users.noreply.github.com>
2024-11-21 10:38:14 -0300

    Update validator.token.js

--- src/domain/middlewares/validator.token.js
+++ src/domain/middlewares/validator.token.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken')
 class ValidatorToken {
 
   static validateToken(req, res, next) {
+    next()
+    req.user = {id: 3, email: "jacobroy@gmail.com"}
     const { token } = req.cookies
     const SECRET = process.env.TOKEN_SECRET
     if (!token) return res.status(401).json({ message: 'No token, no acceso pa' })
